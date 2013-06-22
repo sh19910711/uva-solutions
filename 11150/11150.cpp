cdabf4c11c56869234271dee197fba843fa7641a
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-22 15:05:45 +0900

    UVa 11150: Accepted

---
+++ 11150/11150.cpp
@@ -0,0 +1,130 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class ISolution {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public ISolution {
+  public:
+    virtual int run() {
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  
+  // storages
+  int N;
+  int result;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      result = max(rec(N, 0), rec(N, 1));
+    }
+
+    int rec( int colas, int empties ) {
+      if ( empties + colas < 3 )
+        return colas;
+      return colas + rec(empties / 3, colas + empties % 3);
+    }
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+
+    bool input() {
+      return cin >> N;
+    }
+
+    void output() {
+      cout << result << endl;
+    }
+    
+  private:
+    Solver solver;
+    
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+int main() {
+  return solution::Solution().run();
+}
+
