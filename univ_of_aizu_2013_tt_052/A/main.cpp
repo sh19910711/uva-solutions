370c64bd482113eb13eea3cb307d50d8a15ff69f
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-10 19:15:56 +0900

    LiveArchive 2434: Unsubmitted

---
+++ univ_of_aizu_2013_tt_052/A/main.cpp
@@ -0,0 +1,278 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/define_classes.cpp>
+namespace solution {
+  class Solution;
+  class Solver;
+  class Storages;
+}
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual ~SolutionInterface() {};
+    virtual int run() = 0;
+    
+  protected:
+    SolutionInterface() {}
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input( Storages& s ) { return false; };
+    virtual void output( const Storages& s ) const {};
+    virtual void before_action( const int& test_no ) const {}
+    virtual void after_action( const int& test_no ) const {}
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual ~SolutionBase() {}
+    virtual int run() {
+      this->pre_calc();
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      return 0;
+    }
+  protected:
+    SolutionBase() {}
+  };
+}
+
+// @snippet<sh19910711/contest:misc/smart_pointer_reference_count.cpp>
+namespace misc {
+  class SmartPointerReferenceCount {
+  public:
+    void add() { count ++; }
+    int release() { return -- count; }
+  private:
+    int count;
+  };
+}
+
+// @snippet<sh19910711/contest:misc/smart_pointer.cpp>
+namespace misc {
+  template <typename Type> class SmartPointer {
+  public:
+    SmartPointer(): pointer(0), reference(0) {
+      reference = new misc::SmartPointerReferenceCount();
+      reference->add();
+    }
+    SmartPointer(Type* value): pointer(value) {
+      reference = new misc::SmartPointerReferenceCount();
+      reference->add();
+    }
+    SmartPointer(const SmartPointer<Type>& sp): pointer(sp.pointer), reference(sp.reference) {
+      reference->add();
+    }
+    ~SmartPointer() {
+      if ( reference->release() == 0 ) {
+        delete pointer;
+        delete reference;
+      }
+    }
+    Type& operator *() { return *pointer; }
+    const Type& operator *() const { return *pointer; }
+    Type* operator +( const int& x ) { return pointer + x; }
+    const Type* operator +( const int& x ) const { return pointer + x; }
+    Type* operator ->() { return pointer; }
+    SmartPointer<Type>& operator = ( const SmartPointer<Type>& sp ) {
+      if ( this != &sp ) {
+        if ( reference->release() == 0 ) {
+          delete pointer;
+          delete reference;
+        }
+        pointer = sp.pointer;
+        reference = sp.reference;
+        reference->add();
+      }
+      return *this;
+    }
+  private:
+    Type* pointer;
+    misc::SmartPointerReferenceCount* reference;
+  };
+}
+
+// @snippet<sh19910711/contest:storage/vector.cpp>
+namespace storage {
+  template <typename ValueType, int SIZE> class VectorClass {
+  public:
+    typedef misc::SmartPointer<ValueType> Pointer;
+    VectorClass() { data = Pointer(new ValueType[SIZE]); }
+    ValueType& operator [] ( const int& index ) { return *(data + index); }
+    const ValueType& operator [] ( const int& index ) const { return *(data + index); }
+  private:
+    Pointer data;
+  };
+}
+
+// @snippet<sh19910711/contest:storage/vector_types.cpp>
+namespace storage {
+  const int VECTOR_NONE = -1;
+  template <typename ValueType, int SIZE_X = VECTOR_NONE, int SIZE_Y = VECTOR_NONE, int SIZE_Z = VECTOR_NONE> struct Vector {
+    typedef storage::VectorClass<ValueType, SIZE_Z> Type__;
+    typedef storage::VectorClass<Type__, SIZE_Y> Type_;
+    typedef storage::VectorClass<Type_, SIZE_X> Type;
+  };
+  template <typename ValueType, int SIZE_X, int SIZE_Y> struct Vector<ValueType, SIZE_X, SIZE_Y, VECTOR_NONE> {
+    typedef storage::VectorClass<ValueType, SIZE_Y> Type_;
+    typedef storage::VectorClass<Type_, SIZE_X> Type;
+  };
+  template <typename ValueType, int SIZE_X> struct Vector<ValueType, SIZE_X, VECTOR_NONE, VECTOR_NONE> {
+    typedef storage::VectorClass<ValueType, SIZE_X> Type;
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+  
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/consts-area.cpp>
+namespace solution {
+  // constant vars
+  const int SIZE = 30000 + 11;
+  
+}
+
+namespace solution {
+  struct Time {
+    int oh;
+    int om;
+    int ah;
+    int am;
+
+    int get_late_time() const {
+      int ot = oh * 60 + om;
+      int at = ah * 60 + am;
+      return std::max(0, at - ot);
+    }
+    
+    friend istream& operator >>( istream& is, Time& t ) {
+      return is >> t.oh >> t.om >> t.ah >> t.am;
+    }
+  };
+  typedef storage::Vector<Time, SIZE>::Type Times;
+  
+  struct InputStorages {
+    Int N;
+    Times T;
+  };
+  
+  struct DataStorages {
+  };
+  
+  struct OutputStorages {
+    int result;
+  };
+  
+  struct Storages {
+    InputStorages in;
+    DataStorages data;
+    OutputStorages out;
+  };
+  
+  Storages global_storages;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    OutputStorages& solve( const InputStorages& in, DataStorages& data, OutputStorages& out ) {
+      out.result = calc_average_time(in.N, in.T);
+      return out;
+    }
+
+  protected:
+    static int calc_average_time( const int& N, const Times& T ) {
+      int sum = 0;
+      for ( int i = 0; i < N; ++ i )
+        sum += T[i].get_late_time();
+      return sum / N;
+    }
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+    Solution() {}
+    Solution(Storages* p): storages(p) {}
+    
+  protected:
+    virtual bool action() {
+      if ( ! this->input(this->storages->in) )
+        return false;
+      this->output(solver.solve(this->storages->in, this->storages->data, this->storages->out));
+      return true;
+    }
+
+    bool input( InputStorages& s ) {
+      if ( ! ( std::cin >> s.N ) )
+        return false;
+      for ( int i = 0; i < s.N; ++ i ) {
+        std::cin >> s.T[i];
+      }
+      return true;
+    }
+
+    void output( OutputStorages& s ) {
+      std::cout << s.result << endl;
+    }
+    
+  private:
+    Solver solver;
+    Storages* storages;
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  std::cin.sync_with_stdio(false);
+  return solution::Solution(&solution::global_storages).run();
+}
+#endif
+
+
