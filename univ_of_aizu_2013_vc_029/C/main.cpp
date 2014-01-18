da324d4657ee1d2879477637303d6a6aa7b00acb
Hiroyuki Sano <sh19910711@gmail.com>
2014-01-18 16:15:34 +0900

    UVa 10195: Accepted

---
+++ univ_of_aizu_2013_vc_029/C/main.cpp
@@ -0,0 +1,178 @@
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/define_classes.cpp>
+namespace solution {
+  class Solution;
+  class Solver;
+  struct InputStorage;
+  struct OutputStorage;
+  struct DataStorage;
+  class Storages;
+}
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual ~SolutionInterface() {};
+    virtual int run() = 0;
+    
+  protected:
+    SolutionInterface() {}
+    virtual bool action() = 0;
+    virtual bool input( InputStorage& s ) { return false; };
+    virtual void output( const OutputStorage& s ) const {};
+    virtual void before_all_actions() {}
+    virtual void after_all_actions() {}
+    virtual void before_action( const int& test_no ) const {}
+    virtual void after_action( const int& test_no ) const {}
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual ~SolutionBase() {}
+    virtual int run() {
+      this->before_all_actions();
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      this->after_all_actions();
+      return 0;
+    }
+  protected:
+    SolutionBase() {}
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  using namespace std;
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+  
+}
+
+// @snippet<sh19910711/contest:solution/consts-area.cpp>
+namespace solution {
+  // constant vars
+  
+  
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  
+}
+
+// @snippet<sh19910711/contest:solution/storages-area.cpp>
+namespace solution {
+  struct InputStorage {
+    Double A;
+    Double B;
+    Double C;
+  };
+  
+  struct OutputStorage {
+    Double result;
+  };
+  
+  struct DataStorage {
+  };
+  
+  struct Storages {
+    InputStorage  in;
+    OutputStorage out;
+    DataStorage   data;
+  };
+  
+  Storages global_storages;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    const OutputStorage& solve( const InputStorage& in, OutputStorage& out, DataStorage& data ) {
+      out.result = get_max_radius(in.A, in.B, in.C);
+      return out;
+    }
+    
+  protected:
+    static Double get_max_radius( const Double& A, const Double& B, const Double& C ) {
+      if ( A + B + C < 1e-9 ) {
+        return 0.0;
+      }
+      Double s = ( A + B + C ) / 2.0;
+      Double S = std::sqrt(s * ( s - A ) * ( s - B ) * ( s - C ));
+      Double res = S / s;
+      return res;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+    Solution() {}
+    Solution(Storages* p): storages(p) {}
+    
+  protected:
+    virtual bool action() {
+      if ( ! this->input(this->storages->in) )
+        return false;
+      this->output(solver.solve(this->storages->in, this->storages->out, this->storages->data));
+      return true;
+    }
+
+    bool input( InputStorage& in ) {
+      return std::cin >> in.A >> in.B >> in.C;
+    }
+
+    void output( const OutputStorage& out ) const {
+      std::printf("The radius of the round table is: %.3Lf\n", out.result);
+    }
+    
+  private:
+    Solver solver;
+    Storages* storages;
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  std::cin.sync_with_stdio(false);
+  return solution::Solution(&solution::global_storages).run();
+}
+#endif
+
