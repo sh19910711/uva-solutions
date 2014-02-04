9e2fa77a678848dc59f07a31d5c1068a7486ded5
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-04 23:56:28 +0900

    edit

--- univ_of_aizu_2013_vc_035/D/main.cpp
+++ univ_of_aizu_2013_vc_035/D/main.cpp
@@ -153,32 +153,34 @@ namespace solution {
 
 // @snippet<sh19910711/contest-base:solution/template-solver-area.cpp>
 namespace solution {
-  int box_id_list[24][6] = {
-    {0, 1, 2, 3, 4, 5}, 
-    {0, 2, 4, 1, 3, 5}, 
-    {0, 3, 1, 4, 2, 5}, 
-    {0, 4, 3, 2, 1, 5}, 
-    {1, 0, 3, 2, 5, 4}, 
-    {1, 2, 0, 5, 3, 4}, 
-    {1, 3, 5, 0, 2, 4}, 
-    {1, 5, 2, 3, 0, 4}, 
-    {2, 0, 1, 4, 5, 3}, 
-    {2, 1, 5, 0, 4, 3}, 
-    {2, 4, 0, 5, 1, 3}, 
-    {2, 5, 4, 1, 0, 3}, 
-    {3, 0, 4, 1, 5, 2}, 
-    {3, 1, 0, 5, 4, 2}, 
-    {3, 4, 5, 0, 1, 2}, 
-    {3, 5, 1, 4, 0, 2}, 
-    {4, 0, 2, 3, 5, 1}, 
-    {4, 2, 5, 0, 3, 1}, 
-    {4, 3, 0, 5, 2, 1}, 
-    {4, 5, 3, 2, 0, 1}, 
-    {5, 1, 3, 2, 4, 0}, 
-    {5, 2, 1, 4, 3, 0}, 
-    {5, 3, 4, 1, 2, 0}, 
-    {5, 4, 2, 3, 1, 0}
-  };
+  namespace box_helpers {
+    int id_list[24][6] = {
+      {0, 1, 2, 3, 4, 5}, 
+      {0, 2, 4, 1, 3, 5}, 
+      {0, 3, 1, 4, 2, 5}, 
+      {0, 4, 3, 2, 1, 5}, 
+      {1, 0, 3, 2, 5, 4}, 
+      {1, 2, 0, 5, 3, 4}, 
+      {1, 3, 5, 0, 2, 4}, 
+      {1, 5, 2, 3, 0, 4}, 
+      {2, 0, 1, 4, 5, 3}, 
+      {2, 1, 5, 0, 4, 3}, 
+      {2, 4, 0, 5, 1, 3}, 
+      {2, 5, 4, 1, 0, 3}, 
+      {3, 0, 4, 1, 5, 2}, 
+      {3, 1, 0, 5, 4, 2}, 
+      {3, 4, 5, 0, 1, 2}, 
+      {3, 5, 1, 4, 0, 2}, 
+      {4, 0, 2, 3, 5, 1}, 
+      {4, 2, 5, 0, 3, 1}, 
+      {4, 3, 0, 5, 2, 1}, 
+      {4, 5, 3, 2, 0, 1}, 
+      {5, 1, 3, 2, 4, 0}, 
+      {5, 2, 1, 4, 3, 0}, 
+      {5, 3, 4, 1, 2, 0}, 
+      {5, 4, 2, 3, 1, 0}
+    };
+  }
 
   template <class FaceType> struct Box {
     int cur;
@@ -190,31 +192,25 @@ namespace solution {
       this->faces[x] = type;
     }
 
-    FaceType* get_faces() {
-      FaceType next[6];
-      for ( int i = 0; i < 6; ++ i )
-        next[i] = faces[box_id_list[cur][i]];
-      for ( int i = 0; i < 6; ++ i )
-        this->faces[i] = next[i];
-      return this->faces;
+    int get_index( const int& x ) {
+      return box_helpers::id_list[cur][x];
     }
 
     bool next() {
       if ( this->cur + 1 >= 0 && this->cur + 1 < 24 ) {
         this->cur += 1;
-        this->get_faces();
         return true;
       }
       return false;
     }
 
     FaceType& operator[]( const int& x ) {
-      return faces[x];
+      return faces[get_index(x)];
     }
 
     friend std::ostream& operator <<( std::ostream& os, Box<FaceType>& box ) {
       for ( int i = 0; i < 6; ++ i )
-        os << box.faces[i] << ", ";
+        os << box.faces[box.get_index(i)] << ", ";
       return os;
     }
   };
@@ -255,7 +251,6 @@ namespace solution {
       }
       for ( auto i = 0; i < 24; ++ i ) {
         boxes[cur].cur = i;
-        boxes[cur].get_faces();
         rotate(cur + 1);
       }
     }
